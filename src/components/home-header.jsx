e1e382df00e48c4a1505fdfeeefe2e16820424fa
ibqn <evgen.ibqn@gmail.com>
2023-02-27 02:02:07 +0100

    complete home header

--- src/components/home-header.jsx
+++ src/components/home-header.jsx
@@ -1,9 +1,92 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, TextInput, View } from 'react-native'
+import { ASSETS, COLORS, FONTS, SIZES } from '../constants'
 
-export const HomeHeader = () => {
+export const HomeHeader = ({ handleSearch }) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.font,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Image
+          source={ASSETS.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 25 }}
+        />
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={ASSETS.person04}
+            resizeMode="contain"
+            style={{ width: '100%', height: '100%' }}
+          />
+          <Image
+            style={{
+              position: 'absolute',
+              width: 18,
+              height: 18,
+              bottom: -3,
+              right: 0,
+            }}
+            resizeMode="contain"
+            source={ASSETS.badge}
+          />
+        </View>
+      </View>
+
+      <View style={{ marginVertical: SIZES.font }}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}
+        >
+          Hello, Karina 👋
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}
+        >
+          Let's find a masterpiece
+        </Text>
+
+        <View style={{ marginTop: SIZES.font }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: '100%',
+              borderRadius: SIZES.font,
+              alignItems: 'center',
+              paddingHorizontal: SIZES.font,
+              paddingVertical: SIZES.small - 2,
+              backgroundColor: COLORS.gray,
+            }}
+          >
+            <Image
+              source={ASSETS.search}
+              resizeMode="contain"
+              style={{ width: 20, height: 20, marginRight: SIZES.base }}
+            />
+            <TextInput
+              placeholder="Search NFTs"
+              style={{ flex: 1 }}
+              onChangeText={handleSearch}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   )
 }
